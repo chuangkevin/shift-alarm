0d81743259f2faa3fbcdbaec5a641abaeec3836c
Kevin <kevin950805@gmail.com>
2026-09-13 01:52:33 +0800

    Document private provisioning and verified candidate build

---
+++ firmware-next/main/provisioning.example.h
@@ -0,0 +1,6 @@
+#pragma once
+// Copy to provisioning.h (git-ignored), fill the backend DEVICE_TOKEN, then remove this error.
+#error Fill the private provisioning.h before building a device-specific image
+#define PROVISION_BACKEND "http://100.126.226.79:8237"
+#define PROVISION_TOKEN "REPLACE_WITH_BACKEND_DEVICE_TOKEN"
+#define PROVISION_MANAGE "https://alarm.sisihome.org"
